d2a8c273946e7f5cee6955ca8df7606959f23633
RishaanSucksATMC <113069412+RishaanSucksATMC@users.noreply.github.com>
2023-07-10 00:09:21 +0000

    hj

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,75 @@
-export default async function (req, res) {
+import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+import cheerio from "cheerio";
+import { SsmlBuilder } from "ssml-builder";
 
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
+const AKID = process.env['AKID']
+const SAKID = process.env['SAKID']
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: AKID,
+  secretAccessKey: SAKID,
+  region: 'us-east-1'
+});
+
+const Polly = new AWS.Polly();
+const OPENAI_API_KEY = process.env['OPENAI_API_KEY']
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const fetchData = async () => {
+  const result = await axios.get("https://test-threepoint.glitch.me/");
+  return cheerio.load(result.data);
+};
+
+const extractData = ($) => {
+  const data = [];
+  $("body").each((index, element) => {
+    data.push($(element).text());
+  });
+  return data.join(" ");
+};
+
+export default async function(req, res) {
+  const $ = await fetchData();
+  const content = extractData($);
+  const completion = await openai.createChatCompletion({
+    // Replace `gpt-4` with `gpt-3.5-turbo` if you don't have early access to GPT-4
+    model: "gpt-3.5-turbo-0613",
+    messages: [{ "role": "system", "content": `
+  You are Valerie, an Artificial Intelligence designed  to help visitors on the SPRNGPOD backend site. You make some small talk, you are friendly THE SPELLING OF SPRNGPOD IS "SPRNGPOD" and stays the same as I have specified. You speak as if you are speaking on behalf of SPRNGPOD. Think of yourself like a spokesperson. use this data to teach them about SPRNGPOD Backend: ${content}`}].concat(req.body.messages), 
+    temperature:0
   });
 
-    const data = await response.json();
+  const responseText = completion.data.choices[0].message.content;
 
-    res.status(200).json({ result: data })
+const params = {
+  OutputFormat: "mp3",
+  Text: `<speak>${responseText.replace(/SPRNGPOD/g, "SPRINGPOD")}</speak>`,
+  TextType: "ssml",
+  VoiceId: "Amy",
+  Engine: "neural",
+};
+
+
+
+
+  try {
+    const audioResponse = await Polly.synthesizeSpeech(params).promise();
+    const audioBase64 = audioResponse.AudioStream.toString('base64');
+    const audioDataUri = `data:audio/mp3;base64,${audioBase64}`;
+
+    res.status(200).json({
+      result: completion.data.choices[0].message,
+      audioUrl: audioDataUri
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to generate audio' });
+  }
 }
+
+
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ import ReactMarkdown from 'react-markdown'
 import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Home() {
+  const [audioUrl, setAudioUrl] = useState("");
 
   const [userInput, setUserInput] = useState("");
-  const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState([
-    {
-      "message": "Hi there! How can I help?",
-      "type": "apiMessage"
-    }
+    { role: "assistant", content: "Hi there! I'm Valerie, an AI designed to assist you. How can I help?" }
   ]);
 
   const messageListRef = useRef(null);
@@ -33,54 +30,53 @@ export default function Home() {
 
   // Handle errors
   const handleError = () => {
-    setMessages((prevMessages) => [...prevMessages, { "message": "Oops! There seems to be an error. Please try again.", "type": "apiMessage" }]);
+    setMessages((prevMessages) => [...prevMessages, { role: "assistant", content: "Oops, it looks like my response got cut short. Please try sending your message again and I'll do my best to respond in full. Thank you for your patience!" }]);
     setLoading(false);
     setUserInput("");
   }
 
   // Handle form submission
-  const handleSubmit = async(e) => {
-    e.preventDefault();
+  const handleSubmit = async (e) => {
+  e.preventDefault();
 
-    if (userInput.trim() === "") {
-      return;
-    }
-
-    setLoading(true);
-    setMessages((prevMessages) => [...prevMessages, { "message": userInput, "type": "userMessage" }]);
-
-    // Send user question and history to API
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: userInput, history: history }),
-    });
-
-    if (!response.ok) {
-      handleError();
-      return;
+  if (userInput.trim() === "") {
+    return;
   }
 
-    // Reset user input
-    setUserInput("");
-    const data = await response.json();
+  setLoading(true);
+  const context = [...messages, { role: "user", content: userInput }];
+  setMessages(context);
 
-    if (data.result.error === "Unauthorized") {
-      handleError();
-      return;
-    }
+  // Send chat history to API
+  const response = await fetch("/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ messages: context }),
+  });
 
-    setMessages((prevMessages) => [...prevMessages, { "message": data.result.success, "type": "apiMessage" }]);
-    setLoading(false);
-    
-  };
+  if (!response.ok) {
+    handleError();
+    return;
+  }
+
+  // Reset user input
+  setUserInput("");
+
+  const data = await response.json();
+
+  setMessages((prevMessages) => [...prevMessages, { role: "assistant", content: data.result.content }]);
+  setAudioUrl(data.audioUrl); // Store the audio URL
+  setLoading(false);
+};
+
+
 
   // Prevent blank submissions and allow for multiline input
   const handleEnter = (e) => {
     if (e.key === "Enter" && userInput) {
-      if(!e.shiftKey && userInput) {
+      if (!e.shiftKey && userInput) {
         handleSubmit(e);
       }
     } else if (e.key === "Enter") {
@@ -88,83 +84,85 @@ export default function Home() {
     }
   };
 
-  // Keep history in sync with messages
+
   useEffect(() => {
-    if (messages.length >= 3) {
-      setHistory([[messages[messages.length - 2].message, messages[messages.length - 1].message]]);
-    }
-    }, [messages])
+  if (audioUrl) {
+    const audioElement = new Audio(audioUrl);
+    audioElement.play();
+  }
+}, [audioUrl]);
 
   return (
     <>
       <Head>
-        <title>LangChain Chat</title>
-        <meta name="description" content="LangChain documentation chatbot" />
+        <title>Valerie</title>
+        <meta name="description" content="GPT-4 interface" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.topnav}>
-      <div className = {styles.navlogo}>
-    <a href="/">LangChain</a>
-    </div>
-    <div className = {styles.navlinks}>
-    <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a>
-    <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a>
-    </div>
-</div>
-      <main className={styles.main}>
-      <div className = {styles.cloud}>
-        <div ref={messageListRef} className = {styles.messagelist}>
-        {messages.map((message, index) => {
-          return (
-            // The latest message sent by the user will be animated while waiting for a response
-              <div key = {index} className = {message.type === "userMessage" && loading && index === messages.length - 1  ? styles.usermessagewaiting : message.type === "apiMessage" ? styles.apimessage : styles.usermessage}>
-                {/* Display the correct icon depending on the message type */}
-                {message.type === "apiMessage" ? <Image src = "/parroticon.png" alt = "AI" width = "30" height = "30" className = {styles.boticon} priority = {true} /> : <Image src = "/usericon.png" alt = "Me" width = "30" height = "30" className = {styles.usericon} priority = {true} />}
-              <div className = {styles.markdownanswer}>
-                {/* Messages are being rendered in Markdown format */}
-                <ReactMarkdown linkTarget = {"_blank"}>{message.message}</ReactMarkdown>
-                </div>
-              </div>
-          )
-        })}
+        <div className={styles.navlogo}>
+          <a>Valerie</a>
         </div>
-            </div>
-           <div className={styles.center}>
-            
-            <div className = {styles.cloudform}>
-           <form onSubmit = {handleSubmit}>
-          <textarea 
-          disabled = {loading}
-          onKeyDown={handleEnter}
-          ref = {textAreaRef}
-          autoFocus = {false}
-          rows = {1}
-          maxLength = {512}
-          type="text" 
-          id="userInput" 
-          name="userInput" 
-          placeholder = {loading? "Waiting for response..." : "Type your question..."}  
-          value = {userInput} 
-          onChange = {e => setUserInput(e.target.value)} 
-          className = {styles.textarea}
-          />
-            <button 
-            type = "submit" 
-            disabled = {loading}
-            className = {styles.generatebutton}
-            >
-            {loading ? <div className = {styles.loadingwheel}><CircularProgress color="inherit" size = {20}/> </div> : 
-            // Send icon SVG in input field
-            <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
-            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
-          </svg>}
-            </button>
+        <div className={styles.navlinks}>
+
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {messages.map((message, index) => {
+              return (
+                // The latest message sent by the user will be animated while waiting for a response
+                <div key={index} className={message.role === "user" && loading && index === messages.length - 1 ? styles.usermessagewaiting : message.role === "assistant" ? styles.apimessage : styles.usermessage}>
+                  {/* Display the correct icon depending on the message type */}
+                  {message.role === "assistant" ? <Image src="/openai.png" alt="AI" width="30" height="30" className={styles.boticon} priority={true} /> : <Image src="/usericon.png" alt="Me" width="30" height="30" className={styles.usericon} priority={true} />}
+                  <div className={styles.markdownanswer}>
+                    {/* Messages are being rendered in Markdown format */}
+                    <ReactMarkdown linkTarget={"_blank"}>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+        
+        <div className={styles.center}>
+
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                type="text"
+                id="userInput"
+                name="userInput"
+                placeholder={loading ? "Waiting for response..." : "Talk to Valerie..."}
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+                className={styles.textarea}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.generatebutton}
+              >
+                {loading ? <div className={styles.loadingwheel}><CircularProgress color="inherit" size={20} /> </div> :
+                  // Send icon SVG in input field
+                  <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+                  </svg>}
+              </button>
             </form>
-            </div>
-            <div className = {styles.footer}>
-            <p>Powered by <a href = "https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://twitter.com/chillzaza_" target="_blank">Zahid</a>.</p>
-            </div>
+          </div>
+          <div className={styles.footer}>
+            <p>Powered by <a>OpenAI</a></p>
+          </div>
         </div>
       </main>
     </>
